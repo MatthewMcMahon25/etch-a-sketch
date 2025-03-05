ff6da7888febabd26e0338e992d19795c869d215
MatthewMcMahon25 <macmatt5@gmail.com>
2025-03-05 14:18:37 -0500

    Create the draw mechanism
    Created a draw mechanism that recognizes when
    the mouse enters and exits an individual square
    and changes the squares background color to black

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -6,8 +6,34 @@ function createGrid (size) {
     for (i = 0; i < size * size; i++) {
         const square = document.createElement("div");
         square.classList.add("square");
+
+        let hasEntered = false;
+
+        square.addEventListener("mouseenter", function () {
+            hasEntered = true;
+        });
+
+        square.addEventListener("mouseleave", function (e) {
+            if (hasEntered) {
+                e.target.style.background = "black";
+            }
+        });
+
         container.appendChild(square);
     }
 }
 
 createGrid(16);
+
+const body = document.querySelector("body");
+const button = document.createElement("button");
+button.textContent = "Change Grid Size";
+body.insertBefore(button, document.querySelector("h1"));
+
+//button.addEventListener("click", ) => {
+    //user prompt for Grid Size (# between 1 and including 100)
+
+    //enter user prompt into createGrid
+
+    //createGrid needs to be dynamic in that it allows for different sizes
+//}
