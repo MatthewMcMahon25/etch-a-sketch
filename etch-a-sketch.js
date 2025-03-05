be690589bbebc94c0887adbc3d926a6e21907f5b
MatthewMcMahon25 <macmatt5@gmail.com>
2025-03-05 14:46:08 -0500

    Finish project v1
    Added a button that allows you to change the
    amount of square inside the grid. The layout will
    adjust, while the etch-a-sketch remains static.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,9 +3,14 @@ const container = document.querySelector("#container");
 function createGrid (size) {
     container.innerHTML = "";
 
+    let squareSize = 500 / size;
+
     for (i = 0; i < size * size; i++) {
         const square = document.createElement("div");
         square.classList.add("square");
+
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
 
         let hasEntered = false;
 
@@ -23,17 +28,30 @@ function createGrid (size) {
     }
 }
 
-createGrid(16);
+createGrid(50);
 
 const body = document.querySelector("body");
 const button = document.createElement("button");
 button.textContent = "Change Grid Size";
 body.insertBefore(button, document.querySelector("h1"));
 
-//button.addEventListener("click", ) => {
-    //user prompt for Grid Size (# between 1 and including 100)
+button.addEventListener("click", () => {
 
-    //enter user prompt into createGrid
+    let gridSize = parseInt(prompt("Please Enter A Grid Size #"));
 
-    //createGrid needs to be dynamic in that it allows for different sizes
-//}
+    while (gridSize < 1 || gridSize > 100 || isNaN(gridSize)) {
+        if (gridSize < 1) {
+            alert("The number you entered is less than 1. Please Try Again");
+        } else if (gridSize > 100) {
+            alert("The number you entered is greater than 100. Please try again")
+        } else if (isNaN(gridSize)){
+            alert("That is not a valid number. Please enter a number between 1 and 100.");
+        }
+
+        gridSize = parseInt(prompt("Please Enter Grid Size #"));
+    }
+
+    createGrid(gridSize);
+})
+
+//createGrid needs to be dynamic in that it allows for different sizes
